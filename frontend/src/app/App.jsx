2e254f183cfa4dc3aa88941b40c72d71a5cb4f52
MAbdurahman <mdbdrrhm@gmail.com>
2022-03-27 09:10:53 -0500

    Add some styles to index.less - fonts and colors

--- frontend/src/app/App.jsx
+++ frontend/src/app/App.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Button, DatePicker, version } from 'antd';
 
 export default function App() {
 	return (
-		<div>
-			<h2>This is App component</h2>
+		<div className='center-text'>
+			<h1>antd version: {version}</h1>
+			<DatePicker />
+			<Button type='primary' style={{ marginLeft: 8 }}>
+				Primary Button
+			</Button>
 		</div>
 	);
 }
