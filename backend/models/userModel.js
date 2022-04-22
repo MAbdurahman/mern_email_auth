b8f8fc06809a2589791534c487b4b3910250f079
MAbdurahman <mdbdrrhm@gmail.com>
2022-04-21 19:11:16 -0500

    Update models/userModel.js

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -3,11 +3,51 @@ const jwt = require('jsonwebtoken');
 const Joi = require('joi');
 const passwordComplexity = require('joi-password-complexity');
 
+const name_pattern =
+	/^([a-zA-Z-]{2,}\s[a-zA-z]{1,}'?-?[a-zA-Z]{1,}\s?([a-zA-Z]{1,})?)$/i;
+const email_pattern =
+	/^[!A-Z0-9#$&?*^~_%+-]+(\.[A-Z0-9!_%+-^]+)*?@[A-Z0-9-]+([A-Z0-9.-])*\.[A-Z]{2,}$/i;
+
 const userSchema = new mongoose.Schema({
-	name: { type: String, required: true },
-	email: { type: String, required: true },
-	password: { type: String, required: true },
-	isAdmin: { type: Boolean, required: true, default: false},
+	name: {
+		type: String,
+		trim: true,
+		required: [true, 'First and last name is required!'],
+		minlength: [4, 'Name must be at least 4 characters!'],
+		maxLength: [32, 'Name cannot exceed 31 characters!'],
+		match: [name_pattern, 'Enter first and last name!'],
+	},
+	email: {
+		type: String,
+		trim: true,
+		lowercase: true,
+		required: [true, 'Email is required!'],
+		unique: [true, 'Email already exists!'],
+		match: [email_pattern, 'Enter a valid email!'],
+	},
+	password: {
+		type: String,
+		required: [true, 'Password is required!'],
+		minlength: [8, 'Password must be at least 8 characters!'],
+		select: false,
+	},
+	role: {
+		type: String,
+		enum: ['user', 'admin'],
+		default: 'user',
+	},
+	active: {
+		type: Boolean,
+		default: true,
+		select: false,
+	},
+	passwordChangedAt: Date,
+	passwordResetToken: String,
+	passwordResetExpires: Date,
+	createdAt: {
+		type: Date,
+		default: Date.now,
+	},
 	verified: { type: Boolean, default: false },
 });
 
